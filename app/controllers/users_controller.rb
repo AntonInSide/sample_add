48f0184030cbd7b9cadc1dbfaa56af277ed37da6
Arian <anton.belov.95@bk.ru>
2017-02-28 07:02:54 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		flash[:success] = "Welcom to the Task Manager"
+      Sign_in @user
+  		flash[:success] = "Welcome to the Task Manager"
   		redirect_to @user
   	else
   		render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 class User < ActiveRecord::Base
 	has_secure_password						  					# Прохождение тестов для пароля
-	before_save {email.downcase!}								# Переводим email перед сохранением в нижний регистр
-
-	validates :name, presence: true, length: {maximum: 50} 		#Ограничение на длину имени пользователя
+	before_save {self.email = email.downcase}								# Переводим email перед сохранением в нижний регистр
+	before_create :create_remember_token								
+	validates :name, presence: true, length: {maximum: 50} 				#Ограничение на длину имени пользователя
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i 	# Создаем константу в виде регулярного выражения
 	validates :email, presence: true,
 						format: {with: VALID_EMAIL_REGEX}, 		# Проверка формата почты, доступность адрессов
 						uniqueness: {case_sensitive: false}		# Уникальность адрессов
 	validates :password, length: {minimum: 6} 					# Ограничение на минимальную длину пароля
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
 end
